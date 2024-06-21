873bde59abfad55302bddb3dbb5a93a8334faee9
Hanna Abrahem <habrahem@haverford.edu>
2024-06-21 13:43:20 -0700

    made a git ignore

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -1,1 +1,17 @@
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json()) // used for JSON file 
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+}
+)
+
+app.listen (port, () => {
+    console.log(`Server is running at http://localhost:${port}!`);
+})
+
+
